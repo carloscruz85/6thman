18b395747f7b0c26dfb83f37baa838d389292c0d
Carlos Cruz <carloscruz85@gmail.com>
2020-10-19 15:51:51 -0600

    Customs

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./index.scss";
 
 const App = () => {
-  return <div>Welcome</div>;
+  const views = [1,2,3]
+  return <div>
+    <h1>Fixed</h1>
+    {
+      views.map( (i,c)=>{
+        return <div key={i} className="view"><b>View fixed height: 1080px <span>{c}</span></b></div>
+      } )
+    }
+        <h1>Responsive</h1>
+    {
+      views.map( (i,c)=>{
+        return <div key={i} className="view-responsive"><b>View responsive height <span>{c}</span></b></div>
+      } )
+    }
+  </div>;
 };
 
 export default App;
