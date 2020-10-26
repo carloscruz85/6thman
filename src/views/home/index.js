8b9b665251eb68b32d4517899d51b162ec2aa078
Carlos Cruz <carloscruz85@gmail.com>
2020-10-26 14:53:35 -0600

    Contact done

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import Hero from "../../parts/hero-home/";
 import Intro from "../../parts/intro/";
 import Positions from "../../parts/positions/";
-import Clients from "../../parts/clients/"
+import Clients from "../../parts/clients/";
+import Contact from "../../parts/contact/";
 
 const App = () => {
   
   return <div>
       {/* <Hero />
       <Intro />
-      <Positions /> */}
-      <Clients />
+      <Positions /> 
+      <Clients /> */}
+      <Contact />
     </div>;
 };
 
