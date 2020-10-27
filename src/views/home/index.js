dade9929183761debb6609152659a44e945e0769
Carlos Cruz <carloscruz85@gmail.com>
2020-10-27 07:33:37 -0600

    Footer almost done

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -4,15 +4,17 @@ import Intro from "../../parts/intro/";
 import Positions from "../../parts/positions/";
 import Clients from "../../parts/clients/";
 import Contact from "../../parts/contact/";
+import Footer from "../../parts/footer/";
 
 const App = () => {
   
   return <div>
-      {/* <Hero />
+       <Hero />
       <Intro />
       <Positions /> 
-      <Clients /> */}
-      <Contact />
+      <Clients /> 
+      <Contact /> 
+      <Footer />
     </div>;
 };
 
