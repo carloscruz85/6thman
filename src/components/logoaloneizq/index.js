dd5556cd85986170a275af802a6c14f9ca9f6eb0
Carlos Cruz <carloscruz85@gmail.com>
2020-10-30 11:34:41 -0600

    About us intro done

--- src/components/logoaloneizq/index.js
+++ src/components/logoaloneizq/index.js
@@ -1,11 +1,14 @@
 import React, {useState} from "react";
-import Logo from "../../assets/images/logo-6thman.png"
+import {Link} from "react-router-dom";
+import Logo from "../../assets/images/logo-6thman.png";
+import LogoMin from "../../assets/images/logo-6thman-min.png";
 import "./index.scss";
 
 const LogoAloneIzq = () => {
 
     return <div className="logo-alone-izq-container">
-        <img alt="logo" src={Logo} />
+        <Link to="/"><img className="logo-izq-full" alt="logo" src={Logo} /><img className="logo-izq-min" alt="logo" src={LogoMin} /></Link>
+        
     </div>
     
  
