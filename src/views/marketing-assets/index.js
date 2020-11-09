16eba4053a01c0507d5eb1cb2dfacb5bff4e30fd
Carlos Cruz <carloscruz85@gmail.com>
2020-11-09 10:02:42 -0600

    2 ma done

--- src/views/marketing-assets/index.js
+++ src/views/marketing-assets/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MarketingAssetsTop from '../../parts/marketing-assets-top/'
+import MarketingAssets2 from '../../parts/marketing-assets-2/'
 
 const MarketingAssets = () => {
   return (
     <div>
       <MarketingAssetsTop />
+      <MarketingAssets2 />
     </div>
   )
 }
