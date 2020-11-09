8a3218160d3d81631c9f29fe1d04c125130ed20c
Carlos Cruz <carloscruz85@gmail.com>
2020-11-09 10:42:00 -0600

    3 mk done

--- src/views/marketing-assets/index.js
+++ src/views/marketing-assets/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import MarketingAssetsTop from '../../parts/marketing-assets-top/'
 import MarketingAssets2 from '../../parts/marketing-assets-2/'
+import MarketingAssets3 from '../../parts/marketing-assets-3/'
 
 const MarketingAssets = () => {
   return (
     <div>
       <MarketingAssetsTop />
       <MarketingAssets2 />
+      <MarketingAssets3 />
     </div>
   )
 }
