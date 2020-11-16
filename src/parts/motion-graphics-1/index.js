233c87f37e98e05dd293c1f92d41941787adacc9
Carlos Cruz <carloscruz85@gmail.com>
2020-11-16 15:26:26 -0600

    Motion Graphics done

--- src/parts/motion-graphics-1/index.js
+++ src/parts/motion-graphics-1/index.js
@@ -3,8 +3,14 @@ import './index.scss'
 import Menu from '../../components/menu'
 import Logo from '../../components/logoaloneizq'
 import Tv from '../../assets/images/tv-on.png'
+import PcFrame from '../../assets/images/pc-frame.png'
 
 const MotionGraphics = () => {
+  const year = new Date().getFullYear()
+  const goTop = () => {
+    window.scrollTo(0, 0)
+  }
+
   return (
     <div className="motion-graphics-container ">
       <div className="master-container">
@@ -37,59 +43,67 @@ const MotionGraphics = () => {
                 <p>
                   Interested? Please visit our landing page for more information
                 </p>
-                <p>CLICK HERE</p>
+
+                <div className="main-button-container">
+                  <div className="main-button">CLICK HERE</div>
+                </div>
               </div>
             </div>
             <div className="tv">
               <img src={Tv} alt="tv" className="tv-frame" />
               <video autoPlay muted loop className="video">
                 <source
-                  src="https://6thman.us/media/Master_1920x1040_c_name.mp4"
+                  src="https://6thman.us/media/MotionGraphics-Reel.mp4"
                   type="video/mp4"
                 />
               </video>
             </div>
           </div>
         </div>
+      </div>
 
-        {/* <div className="motion-media-container ">
-          <div className="d-flex flex-column-reverse flex-lg-row-reverse content-container">
-            <div className="pc">
-              <img src={Tv} alt="pc" className="pc-frame" />
-              <div className="slider-container">
-                <video autoPlay muted loop className="slider">
+      <div className="animation-master-container-mobile">
+        <div className="container">
+          <div className="row intro-animation">
+            <div className="col-12">
+              <span className="title">MOTION GRAPHICS / ANIMATION</span>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <p>
+                There’s no better way to display, express, or promote your
+                brand’s message than with a visually stunning animated video. A
+                great alternative and more cost effective than its traditional
+                life filming counterpart.
+              </p>
+              <div className="mobile-animation-container">
+                <img src={PcFrame} alt="Pc Frame" />
+                <video autoPlay muted loop className="video">
                   <source
-                    src="http://carloscruz85.com/6thman/media/Master_1920x1040_c_name.mp4"
+                    src="https://6thman.us/media/MotionGraphics-Reel.mp4"
                     type="video/mp4"
                   />
                 </video>
               </div>
-            </div>
+              <p className="no-opa">
+                <ul>
+                  <li>Explain complex ideas quickly</li>
+                  <li>Visually appealing</li>
+                  <li>Easy to upgrade</li>
+                  <li>Bring your vision to life</li>
+                </ul>
+              </p>
+              <p>
+                Interested? Please visit our landing page for more information
+              </p>
 
-            <div className="text">
-              <div className="text-container">
-                <p>
-                  There’s no better way to display, express, or promote your
-                  brand’s message than with a visually stunning animated video.
-                  A great alternative and more cost effective than its
-                  traditional life filming counterpart.
-                </p>
-                <p className="no-opa">
-                  <ul>
-                    <li>Explain complex ideas quickly</li>
-                    <li>Visually appealing</li>
-                    <li>Easy to upgrade</li>
-                    <li>Bring your vision to life</li>
-                  </ul>
-                </p>
-                <p>
-                  Interested? Please visit our landing page for more information
-                </p>
-                <p>CLICK HERE</p>
+              <div className="main-button-container">
+                <div className="main-button">CLICK HERE</div>
               </div>
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
 
       <Menu />
